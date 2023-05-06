8e10fe18268c62cc10cdbb3f0f10af70384212f6
Cesar Alonso Garcia <cesarg324@gmail.com>
2023-05-06 12:20:03 -0500

    Add get request for API

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-notes.get('/notes', (req, res) =>
+notes.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+module.exports = notes;
