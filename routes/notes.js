99f047e31c801fee9814af4e0140c49aa5047704
Cesar Alonso Garcia <cesarg324@gmail.com>
2023-05-06 11:29:29 -0500

    Add file structure and GET Route for retrieving notes

---
+++ routes/notes.js
@@ -0,0 +1,8 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
